5c6a6721fb5100e43e88ebb26310b309c5b55ba0
Hugo O. Rivera <hugo@roguh.com>
2021-07-27 09:08:43 -0700

    Add working implementation of requestMulti()
    - Typescript
    - Make NATS request that expects multiple responses
    - Optimized to return as soon as all NATS responses arrive
    - 100% unit test coverage with Jest
    - Integration tests using Docker compose

---
+++ Makefile
@@ -0,0 +1,34 @@
+CONTAINER_NAME=nats-request-multi-js_request-multi_1
+setup:
+	npm install
+
+scan:
+	npm audit
+
+lint:
+	npm run lint
+
+format:
+	npm run format
+
+check-format:
+	npm run check-format
+
+run-without-docker:
+	docker-compose run -p 4222:4222 -d nats
+	npm run dev
+
+up:
+	docker-compose up --build -d
+
+down:
+	docker-compose down --remove-orphans
+
+test-integration: up
+	sleep 2; docker exec -t ${CONTAINER_NAME} npm run test-integration
+
+test-unit:
+	npm run test-unit
+
+test-unit-in-docker:
+	docker exec -t ${CONTAINER_NAME} sh -c "npm run test-unit"
---
+++ src/requestMulti.ts
@@ -0,0 +1,81 @@
+import Timeout from "await-timeout";
+import { NatsError, createInbox, JSONCodec, Msg, NatsConnection } from "nats";
+
+export const requestMultiCallback =
+  (
+    responses: Msg[],
+    waitAllReceivedResolve: null | ((arg: number) => void),
+    setNatsError: (arg: NatsError) => void,
+    expected: number
+  ) =>
+  (err: NatsError | null, msg: Msg) => {
+    if (err) {
+      setNatsError(err);
+    } else {
+      responses.push(msg);
+      if (waitAllReceivedResolve && responses.length === expected) {
+        waitAllReceivedResolve(expected);
+      }
+    }
+  };
+
+const requestMulti = async (
+  nc: NatsConnection,
+  subject: string,
+  payload: Uint8Array,
+  { timeout = 0.5, expected = 1 }: { timeout?: number; expected?: number } = {}
+): Promise<Msg | Msg[]> => {
+  if (expected < 1) {
+    throw new Error("expected cannot be less than one")
+  }
+
+  if (expected === 1) {
+    return await nc.request(subject, payload, { timeout });
+  }
+
+  const responses: Msg[] = [];
+  let natsError: NatsError | null = null;
+  let waitAllReceivedResolve: ((arg: number) => void) | null = null;
+  const waitAllReceived = new Promise(
+    (resolve) => (waitAllReceivedResolve = resolve)
+  );
+
+  const randomInt = (max: number) => Math.floor(Math.random() * max);
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const randomInboxId = Array(22).fill(0).map(() => chars[randomInt(chars.length)]).join('')
+  const inbox = `_INBOX.${randomInboxId}`;
+
+  const inboxSub = await nc.subscribe(inbox, {
+    max: expected,
+    callback: requestMultiCallback(
+      responses,
+      waitAllReceivedResolve,
+      (err: NatsError) => (natsError = err),
+      expected
+    ),
+  });
+
+  await Timeout.set(10)
+
+  await nc.publish(subject, payload, {
+    reply: inbox,
+  });
+
+  try {
+    await Timeout.wrap(waitAllReceived, timeout, "NATS Timeout");
+  } catch (err) {}
+
+  await inboxSub.unsubscribe();
+
+  if (natsError !== null) {
+    throw natsError;
+  }
+
+  if (responses.length === 0) {
+    throw new Error("NATS multi-request timeout");
+  }
+
+  return responses;
+};
+
+export default requestMulti;

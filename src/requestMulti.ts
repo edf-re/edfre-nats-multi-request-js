6a5ea1ae5388923de7d63f7fe75282edd65bddbc
Janala Dragon <janala@dracoventions.com>
2023-10-28 10:12:42 -0700

    Retry up to 50 times when error 503 ServiceUnavailableError is encountered.

--- src/requestMulti.ts
+++ src/requestMulti.ts
@@ -1,5 +1,28 @@
 import Timeout from "await-timeout";
-import { NatsError, createInbox, JSONCodec, Msg, NatsConnection } from "nats";
+import { NatsError, createInbox, JSONCodec, Msg, NatsConnection, Subscription } from "nats";
+import * as env from "env-var";
+import * as fs from 'fs';
+import { configure, getLogger } from "log4js";
+
+configure({
+  appenders: {
+    console: {
+      type: "console",
+      layout: {
+        type: "pattern",
+        pattern: "%d [%p] %f{1}:%l %m",
+      },
+    },
+  },
+  categories: {
+    default: {
+      appenders: ["console"],
+      level: "debug",
+      enableCallStack: true,
+    },
+  },
+});
+const logger = getLogger();
 
 export const requestMultiCallback =
   (
@@ -10,13 +33,16 @@ export const requestMultiCallback =
   ) =>
   (err: NatsError | null, msg: Msg) => {
     if (err) {
+
       setNatsError(err);
     } else {
       responses.push(msg);
       if (waitAllReceivedResolve && responses.length === expected) {
+        logger.debug("Calling waitAllReceivedResolve");
         waitAllReceivedResolve(expected);
       }
     }
+    logger.debug(`requestMultiCallback responses.length=${responses.length}, subject=${msg.subject}, sid=${msg.sid}, reply=${msg.reply}, data=${msg.data}`);
   };
 
 const requestMulti = async (
@@ -25,56 +51,105 @@ const requestMulti = async (
   payload: Uint8Array,
   { timeout = 0.5, expected = 1 }: { timeout?: number; expected?: number } = {}
 ): Promise<Msg | Msg[]> => {
+  logger.info("requestMulti starting with subject", subject, "payload", payload, "timeout", timeout, "expected", expected);
+
   if (expected < 1) {
     throw new Error("expected cannot be less than one")
   }
 
-  if (expected === 1) {
-    return await nc.request(subject, payload, { timeout });
-  }
-
-  const responses: Msg[] = [];
+  let responses: Msg[] = [];
   let natsError: NatsError | null = null;
   let waitAllReceivedResolve: ((arg: number) => void) | null = null;
-  const waitAllReceived = new Promise(
-    (resolve) => (waitAllReceivedResolve = resolve)
-  );
-
+  let waitAllReceived: Promise<number> | null = null;
   const randomInt = (max: number) => Math.floor(Math.random() * max);
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   const randomInboxId = Array(22).fill(0).map(() => chars[randomInt(chars.length)]).join('')
   const inbox = `_INBOX.${randomInboxId}`;
+  let inboxSub: Subscription | null = null;
 
-  const inboxSub = await nc.subscribe(inbox, {
-    max: expected,
-    callback: requestMultiCallback(
-      responses,
-      waitAllReceivedResolve,
-      (err: NatsError) => (natsError = err),
-      expected
-    ),
-  });
+  if(expected > 1) {
+    waitAllReceived = new Promise(
+      (resolve) => (waitAllReceivedResolve = resolve)
+    );
 
-  await Timeout.set(10)
+    inboxSub = await nc.subscribe(inbox, {
+      max: expected,
+      callback: requestMultiCallback(
+        responses,
+        waitAllReceivedResolve,
+        (err: NatsError) => (natsError = err),
+        expected
+      ),
+    });
 
-  await nc.publish(subject, payload, {
-    reply: inbox,
-  });
+    // Wait 10ms, I guess to ensure the subscription is in place?
+    await Timeout.set(10)
+  }
 
-  try {
-    await Timeout.wrap(waitAllReceived, timeout, "NATS Timeout");
-  } catch (err) {}
+  for(let i=0; i<50; i++) {
+    if (expected === 1) {
+      try {
+        const msg = await nc.request(subject, payload, {timeout});
+        return msg;
+      } catch (error) {
+        if (i < 49 && error instanceof NatsError && error.code === "503") {
+          logger.debug("NATS returned code 503, ServiceUnavailableError.  Trying again.");
+          continue;
+        }
+        logger.error(`nc.request error: ${error} from subject ${subject} with payload '${payload}'`);
+        throw error;
+      }
+    }
+    else {
+      responses.length = 0;
+      await nc.publish(subject, payload, {
+        reply: inbox,
+      });
 
-  await inboxSub.unsubscribe();
+      try {
+        // Wait timeout secs for waitAllReceived to complete.
+        if (waitAllReceived) {
+          await Timeout.wrap(waitAllReceived, timeout * 1000, "NATS Timeout");
+          logger.debug("Done with waitAllReceived.");
+        }
+      } catch (error) {
+        if (error instanceof NatsError) {
+          natsError = error;
+        } else {
+          let errStr = "Unknown";
+          if (typeof error === "string") {
+            errStr = error;
+          } else if (error && typeof error === "object" && "message" in error) {
+            errStr = (error as { message: string }).message;
+          }
+          logger.error("multiRequest error:", errStr, "from subject", subject, "with payload", payload);
+          throw error;
+        }
+      }
 
-  if (natsError !== null) {
-    throw natsError;
+      if (i < 49 && natsError && natsError.code === "503") {
+        // It's not ideal to resend the message to all subscribers when only one failed to
+        // respond, but there is no way to know which subscriber failed or to re-send to only one
+        // subscriber.
+        logger.debug("NATS returned code 503, ServiceUnavailableError.  Trying again.");
+        continue;
+      }
+      //waitAllReceivedResolve(expected);
+      if (inboxSub) {
+        await inboxSub.unsubscribe();
+      }
+      if (natsError !== null) {
+        throw natsError;
+      }
+      break;
+    }
   }
 
   if (responses.length === 0) {
     throw new Error("NATS multi-request timeout");
   }
 
+  logger.debug(`NATS requestMulti returning ${responses}`);
   return responses;
 };
 
